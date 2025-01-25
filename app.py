932b95f5a514e455f677bff8f140123bd5a6099f
Diya <dn03cbk@gmail.com>
2025-01-25 20:18:13 +0530

    imported packages

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-meow
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+drawing_history = []
+current_state = []
